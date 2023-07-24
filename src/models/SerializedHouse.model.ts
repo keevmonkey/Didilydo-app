3349d5de75d0dfd859d79651a4ec9a009f98f5a8
Kevin Hui <kevinhuitaylors@gmail.com>
2023-07-24 23:14:01 +0800

    created model for serialized task and house, task store

---
+++ src/models/SerializedHouse.model.ts
@@ -0,0 +1,25 @@
+export interface SerializedHouse {
+  id: string
+  type: string
+  attributes: HouseAttributes
+  links: HouseLinks
+  relationships: HouseRelationships
+}
+
+export type HouseAttributes = {
+  name: string
+  description: string
+  address: string
+  settings: any
+}
+
+export type HouseLinks = {
+  avatar: string | null
+}
+
+export type HouseRelationships = {
+  tenancies: { data: [] }
+  users: { data: [] }
+  taskLists: { data: [] }
+  tasks: { data: [] }
+}
