d31f79ff370d57438e86445c89e02981861ac894
Kevin Hui <kevinhuitaylors@gmail.com>
2023-07-28 19:39:26 +0800

    fleshed out account tasks index

--- src/views/pages/account/tasks/TasksIndex.vue
+++ src/views/pages/account/tasks/TasksIndex.vue
@@ -1,12 +1,27 @@
 <template>
   <div>
-    <v-card> Task Index </v-card>
+    <v-list-item title="Task Index"></v-list-item>
+
+    <v-list>
+      <v-list-item title="A new task" append-icon="mdi-plus" @click="''" variant="tonal">
+      </v-list-item>
+
+      <div v-for="n in 4" :key="n">
+        <v-list-item class="py-3">
+          <v-list-item-title>A special task</v-list-item-title>
+          <v-list-item-subtitle>Click me for pop up</v-list-item-subtitle>
+          <template #append>
+            <v-btn icon variant="text">
+              <v-icon color="indigo">mdi-eye</v-icon>
+            </v-btn>
+          </template>
+        </v-list-item>
+        <v-divider />
+      </div>
+    </v-list>
   </div>
 </template>
 
 <script setup lang="ts">
-import {ref} from 'vue'
-
-  
-
+import { ref } from 'vue'
 </script>
