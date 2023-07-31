dd5c4d5e16e3823fe12481ae42d3eee15f08fdfd
Kevin Hui <kevinhuitaylors@gmail.com>
2023-07-31 11:50:56 +0800

    dashboard overview tasks links to task page

--- src/views/pages/house/dashboard/components/DashboardOverview.vue
+++ src/views/pages/house/dashboard/components/DashboardOverview.vue
@@ -10,7 +10,7 @@
         </v-card>
       </v-col>
       <v-col cols="6">
-        <v-card variant="tonal" class="ma-2">
+        <v-card variant="tonal" class="ma-2" @click="travelTo('house-tasks')">
           <v-card-title>Latest Tasks</v-card-title>
           <v-card-text>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, adipisci!
@@ -36,3 +36,17 @@
     </v-row>
   </div>
 </template>
+
+<script setup lang="ts">
+import { useRouter, useRoute } from 'vue-router'
+
+const router = useRouter()
+const route = useRoute()
+
+const travelTo = (name: string) => {
+  router.push({
+    name: name,
+    params: { slug: route.params.slug }
+  })
+}
+</script>
